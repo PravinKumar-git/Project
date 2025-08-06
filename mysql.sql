418d1557008ace6fe328ac9304e82dc212557ed3
PravinKumar-git <pravinshanmugam27@gmail.com>
2025-08-06 14:52:51 +0530

    fot

--- mysql.sql
+++ mysql.sql
@@ -1,5 +1,10 @@
-package java;
-
-public class mysql {
-    khgihdrhgoishdfgo;
-}
+create database jdb;
+use java;
+create table student (
+ID int,
+name varchar(67)
+);
+insert into student values(1,"abi");
+describe table student;
+drop table student;
+select*from student;
